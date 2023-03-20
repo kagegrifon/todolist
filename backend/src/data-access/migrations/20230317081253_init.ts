3a0f967dde9f4ee66c7299f0ebaa3d1b7673ec65
Grigorii Veinin <kage.grifon@yandex.ru>
2023-03-20 20:11:16 +0700

    [backend][refactor] add entity folder and move todo, user

--- backend/src/data-access/migrations/20230317081253_init.ts
+++ backend/src/data-access/migrations/20230317081253_init.ts
@@ -1,7 +1,7 @@
 import { DB_TABLE_NAME } from '../tableNames'
 import { Knex } from 'knex'
-import { createTodoSchema } from 'todo/todo.schema'
-import { createUserSchema } from 'auth/user.schema'
+import { createTodoSchema } from 'entity/todo/todo.schema'
+import { createUserSchema } from 'entity/auth/user.schema'
 
 export async function up(knex: Knex): Promise<void> {
     await createUserSchema(knex)
