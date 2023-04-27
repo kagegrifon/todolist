ead13efdddd9ee14f55b83439fa6ed211f8d7841
Grigorii Veinin <kage.grifon@yandex.ru>
2023-04-27 14:56:48 +0600

    [backend] start imp registrate

--- backend/src/entity/auth/auth.service.ts
+++ backend/src/entity/auth/auth.service.ts
@@ -1,16 +1,32 @@
 import { TypicalCRUDService } from 'shared/service'
 import { authModel } from './auth.model'
 import { AuthModelAbstract, AuthServiceAbstract, IAuth, IUserLogin, IUserSignUp } from './type'
+import { UserModelAbstract } from 'entity/user/type'
+import { userService } from 'entity/user/user.service'
 
 class AuthService extends TypicalCRUDService<IAuth> implements AuthServiceAbstract {
     model: AuthModelAbstract
+    userService: UserModelAbstract
 
-    constructor(model: AuthModelAbstract) {
+    constructor(model: AuthModelAbstract, userService: UserModelAbstract) {
         super(model)
+        this.userService = userService
     }
 
     async registrate(data: IUserSignUp) {
-        console.log({ data })
+        const isEmailExist = await this.userService.findByEmail(data.email)
+        if (isEmailExist) {
+            throw Error(`Email ${data.email} is already exist`)
+        }
+
+        const newUser = await this.userService.create({ email: data.email, name: data.name })
+        const newAuth = await this.model.create({
+            isActivated: false,
+            activationLink: '??? TODO',
+            password: data.password,
+            userId: newUser.id,
+        })
+        // сгенерировать activationLink, захешировать пароль, отправить письмо на почту
         return undefined
     }
 
@@ -35,4 +51,4 @@ class AuthService extends TypicalCRUDService<IAuth> implements AuthServiceAbstra
     }
 }
 
-export const authService = new AuthService(authModel)
+export const authService = new AuthService(authModel, userService)
